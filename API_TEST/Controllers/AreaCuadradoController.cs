d48870d403d5b4db5b2ffc4f628dfeb44d44346a
Brandon Lozada <brandon_lozada@outlook.com>
2023-03-01 22:20:56 -0600

    Subir ejercicio 8.

---
+++ API_TEST/Controllers/AreaCuadradoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaCuadradoController : Controller
+    {
+        [HttpGet] // Ejercicio 8
+        [Route("AreaCuadrado/{Lado:double}")]
+        public IActionResult AreaCuadrado(double Lado)
+        {
+            String Resultado = "El área del cuadrado es : " + (Lado * Lado);
+
+            return StatusCode(StatusCodes.Status200OK, Resultado);
+        }
+    }
+}
