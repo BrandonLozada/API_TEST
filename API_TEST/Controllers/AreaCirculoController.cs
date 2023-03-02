e5d52550d0097f2c4a492c35ee4c60f7d8a6c036
Brandon Lozada <brandon_lozada@outlook.com>
2023-03-01 22:38:19 -0600

    Subir ejercicio 10.

---
+++ API_TEST/Controllers/AreaCirculoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaCirculoController : Controller
+    {
+        [HttpGet] // Ejercicio 10
+        [Route("AreaCirculo/{Radio:double}")]
+        public IActionResult AreaCirculo(double Radio)
+        {
+            double Resultado = Math.PI * (Radio * Radio);
+
+            string Mensaje = "El área del círculo es: " + Resultado;
+
+            return StatusCode(StatusCodes.Status200OK, Mensaje);
+        }
+    }
+}
