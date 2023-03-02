2614a7b6f1e724b514bb4f37c4ecadeddd134c09
Brandon Lozada <brandon_lozada@outlook.com>
2023-03-01 22:16:22 -0600

    Subir ejercicio 7.

---
+++ API_TEST/Controllers/ElevarAlCuadradoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ElevarAlCuadradoController : Controller
+    {
+        [HttpGet] //Ejercicio 7
+        [Route("ElevarAlCuadrado/{Num:double}")]
+        public IActionResult ElevarAlCuadrado(double Num)
+        {
+            double Resultado = Math.Pow(Num, 2);
+            String Mensaje = "El número: " + Num + " elevado al cuadrado es " + Resultado;
+
+            return StatusCode(StatusCodes.Status200OK, Mensaje);
+        }
+    }
+}
