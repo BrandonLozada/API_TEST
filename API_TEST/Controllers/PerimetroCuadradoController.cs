1e0b3aae7cf6bf000c6c2278f759c0d700b7580f
Brandon Lozada <brandon_lozada@outlook.com>
2023-03-01 22:26:35 -0600

    Subir ejercicio 9; Modificar mensaje de salida del ejercicio 8.

---
+++ API_TEST/Controllers/PerimetroCuadradoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PerimetroCuadradoController : Controller
+    {
+        [HttpGet] // Ejercicio 9
+        [Route("PerimetroCuadrado/{Lado:double}")]
+        public IActionResult PerimetroCuadrado(double Lado)
+        {
+            String Resultado = "El perímetro del cuadrado es: " + (Lado * 4);
+
+            return StatusCode(StatusCodes.Status200OK, Resultado);
+        }
+    }
+}
