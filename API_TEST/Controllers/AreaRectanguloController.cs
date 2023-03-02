2cf6ea23da06cd822a7b64204e4bcc71180f9f86
Brandon Lozada <brandon_lozada@outlook.com>
2023-03-01 22:48:00 -0600

    Subir ejercicio 12.

---
+++ API_TEST/Controllers/AreaRectanguloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_TEST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaRectanguloController : Controller
+    {
+        [HttpGet] // Ejercicio 12
+        [Route("AreaRectangulo/{Base:double}/{Altura:double}")]
+        public IActionResult AreaRectangulo(double Base, double Altura)
+        {
+            string Resultado = "El área del rectángulo es: " + (Base * Altura);
+
+            return StatusCode(StatusCodes.Status200OK, Resultado);
+        }
+    }
+}
